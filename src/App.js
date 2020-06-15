43597d55b00c60437a3b09c7b3e143065bdc3073
shalanah <shalanahfaith@gmail.com>
2020-06-15 16:27:19 -0500

    Remove unused comp

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { GlobalStyles } from "./components/GlobalStyles";
 import styled from "styled-components";
 import { colors } from "./utils/colors";
 import Swatch from "./components/Swatch";
-import Form from "./components/Form";
 import { filterOpts, sortOpts, sortColors, filterColors } from "./utils/lib";
 
 const App = () => {
