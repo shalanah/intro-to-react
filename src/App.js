6fd14d82c1f0f75e8540c030bd88bf010e221172
shalanah <shalanahfaith@gmail.com>
2020-06-16 12:54:50 -0500

    lazy message

--- src/App.js
+++ src/App.js
@@ -5,12 +5,37 @@ import { colors } from "./utils/colors";
 import Swatch from "./components/Swatch";
 import { filterOpts, sortOpts, sortColors, filterColors } from "./utils/lib";
 
+const H1 = styled.h1`
+  display: inline-block;
+  font-size: 8rem;
+  background: linear-gradient(45deg, red, orange, yellow, green, blue, violet);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  padding: 0.25rem;
+  transition: 0.2s;
+  :hover {
+    font-size: 20rem;
+  }
+`;
+
 const App = () => {
   return (
-    <div>
+    <>
       <GlobalStyles />
-      <h1>Colors</h1>
-    </div>
+      <H1>Colors</H1>
+      <div
+        style={{
+          border: "1px solid #000",
+          borderRadius: "10px",
+          overflow: "hidden",
+        }}
+      >
+        {colors.map((color, i) => {
+          return <Swatch color={color} key={i} />;
+        })}
+      </div>
+    </>
   );
 };
 
--- src/components/Swatch.js
+++ src/components/Swatch.js
@@ -2,8 +2,25 @@ import React from "react";
 import styled from "styled-components";
 import { getTextColor } from "../utils/lib";
 
-const Swatch = () => {
-  return null;
+const Container = styled.div`
+  height: 45px;
+  line-height: 45px;
+  padding-left: 15px;
+  font-size: 1.3rem;
+  text-align: left;
+`;
+
+const Swatch = ({ color }) => {
+  return (
+    <Container
+      style={{
+        background: color,
+        color: getTextColor(color),
+      }}
+    >
+      {color}
+    </Container>
+  );
 };
 
 export default Swatch;
